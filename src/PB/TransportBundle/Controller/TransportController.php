6c6e4992fbbab9f411d0ca90879b2016f8d878d4
patbal <supatbal@yahoo.fr>
2017-08-06 13:09:47 +0200

    Fonctionnel Correction de TransportController

--- src/PB/TransportBundle/Controller/TransportController.php
+++ src/PB/TransportBundle/Controller/TransportController.php
@@ -24,6 +24,11 @@ use FOS\UserBundle\Model\UserInterface;
 
 class TransportController extends Controller
 {
+
+    /**
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction($page)
     {
 	  
@@ -55,210 +60,6 @@ class TransportController extends Controller
 
     }
 
-
-    // *************************************************************************************************
-    // **************************************** CONTACTS ***********************************************
-    // *************************************************************************************************
-
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function addContactAction(Request $request)
-    {
-    	$contact = new Contact();
-		$form = $this -> get('form.factory')->create(ContactType::class, $contact);
-
-		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-			$em = $this->getDoctrine()->getManager();
-			$em -> persist($contact);
-			$em -> flush();
-
-			$request->getSession()->getFlashBag()->add('notice', 'Contact ajouté');
-
-			return $this->redirectToRoute('pb_transport_homepage');
-		}
-
-		return $this->render('PBTransportBundle:Transport:editContact.html.twig', array('form' => $form->createView(), 'titre' => "Ajout de contact", 'boutonDel' => false));
-    }
-
-    /**
-     * @param Contact $contact
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function viewContactAction(Contact $contact, $id)
-    {
-    	if (null === $contact) {
-      		throw new NotFoundHttpException("Ce contact n'existe pas !");
-    	}
-
-    	return $this->render('PBTransportBundle:Transport:viewContact.html.twig', array(
-    		'contact'	=> $contact));
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function viewContactsAction()
-    {
-        $listContacts = $this -> getDoctrine()->getRepository('PBTransportBundle:Contact')->getContacts();
-        return $this -> render('PBTransportBundle:Transport:viewContacts.html.twig', array('contacts' => $listContacts));
-    }
-
-    public function editContactAction(Contact $contact, $id, Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $form = $this -> get('form.factory')->create(ContactType::class, $contact);
-
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-
-            $em->persist($contact);
-            $em->flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Modification de contact effectuée');
-
-            return $this->redirectToRoute('pb_transport_viewcontacts');
-        }
-
-        return $this->render('PBTransportBundle:Transport:editContact.html.twig', ['contact' => $contact, 'form' => $form->createView(), 'titre' => "Modification de contact", 'boutonDel' => true]);
-    }
-
-    public function deleteContactAction(Contact $contact, $id, Request $request)
-    {
-        if (null === $contact)
-        {
-            throw new NotFoundHttpException("contact inexistant en base de donnée");
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $em -> remove($contact);
-        $em -> flush();
-
-        $request -> getSession() -> getFlashbag() -> add('alert', 'Contact Supprimé');
-        return $this -> redirectToRoute('pb_transport_homepage');
-    }
-
-    // *************************************************************************************************
-    // **************************************** TRANSPORTEURS ******************************************
-    // *************************************************************************************************
-
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function addTransporteurAction(Request $request)
-    {
-        $transporteur = new Transporteur();
-        $form = $this -> get('form.factory')->create(TransporteurType::class, $transporteur);
-
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            foreach ($transporteur->getContacts() as $contact) {
-                $contact->setTransporteur($transporteur);
-                $em->persist($contact);
-            }
-
-            $em -> persist($transporteur);
-            $em -> flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Transporteur ajouté');
-
-            return $this->redirectToRoute('pb_transport_viewtransporteurs');
-        }
-
-        return $this->render('PBTransportBundle:Transport:editTransporteur.html.twig', array('form' => $form->createView(), 'titre' => "Ajout de Transporteur", 'boutonDel' => false));
-    }
-
-    /**
-     * @param Transporteur $transporteur
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function viewTransporteurAction(Transporteur $transporteur, $id)
-    {
-        if (null === $transporteur) {
-            throw new NotFoundHttpException("Ce transporteur n'existe pas !");
-        }
-
-        return $this->render('PBTransportBundle:Transport:viewTransporteur.html.twig', array(
-            'transporteur'	=> $transporteur));
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function viewTransporteursAction()
-    {
-        $listTransporteurs = $this -> getDoctrine()->getRepository('PBTransportBundle:Transporteur')->getTransporteurs();
-        return $this -> render('PBTransportBundle:Transport:viewTransporteurs.html.twig', array('transporteurs' => $listTransporteurs));
-    }
-
-
-    /**
-     * @param Transporteur $transporteur
-     * @param $id
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function editTransporteurAction(Transporteur $transporteur, $id, Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $form = $this -> get('form.factory')->create(TransporteurType::class, $transporteur);
-
-
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-
-            // On boucle sur l'entité originale pour en supprimer les contacts Transporteur qui ne sont pas présents dans la nouvelle...
-            $oldContacts = $em->getRepository(Contact::class)->findBytransporteur($transporteur->getId());
-            foreach ($oldContacts as $oldContact) {
-                if (!$transporteur->getContacts()->contains($oldContact)){
-                    $oldContact->setTransporteur(null);
-                    $em->persist($oldContact);
-                }
-            }
-
-            // puis on boucle sur la nouvelle pour lui injecter les contacts sélectionnés
-            foreach ($transporteur->getContacts() as $contact) {
-                $contact->setTransporteur($transporteur);
-                $em->persist($contact);
-            }
-
-
-            $em->persist($transporteur);
-            $em->flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Modification de transporteur effectuée');
-
-            return $this->redirectToRoute('pb_transport_viewtransporteurs');
-        }
-
-        return $this->render('PBTransportBundle:Transport:editTransporteur.html.twig', ['transporteur' => $transporteur, 'form' => $form->createView(), 'titre' => "Modification de transporteur", 'boutonDel' => true]);
-    }
-
-    /**
-     * @param Transporteur $transporteur
-     * @param $id
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function deleteTransporteurAction(Transporteur $transporteur, $id, Request $request)
-    {
-        if (null === $transporteur)
-        {
-            throw new NotFoundHttpException("transporteur inexistant en base de donnée");
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $em -> remove($transporteur);
-        $em -> flush();
-
-        $request -> getSession() -> getFlashbag() -> add('alert', 'Transporteur Supprimé');
-        return $this -> redirectToRoute('pb_transport_homepage');
-    }
-
-
-
     // *************************************************************************************************
     // **************************************** TRANSPORTS *********************************************
     // *************************************************************************************************
@@ -270,7 +71,7 @@ class TransportController extends Controller
     public function addTransportAction(Request $request)
 	{
 		$transport = new Transport();
-		$form = $this -> get('form.factory')->create(TransportBriefType::class, $transport);//type hérité de transport, avec suppression de champs
+		$form = $this -> get('form.factory')->create( TransportBriefType::class, $transport);//type hérité de transport, avec suppression de champs
 
 		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
 			$em = $this->getDoctrine()->getManager();
@@ -379,6 +180,12 @@ class TransportController extends Controller
             'transport'	=> $transport));
     }
 
+    /**
+     * @param Transport $transport
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function sendMailAction(Transport $transport, $id, Request $request)
     {
         $trans = (new \Swift_SmtpTransport($this -> container -> getParameter('mailer_host'), $this -> container -> getParameter('mailer_port')))
